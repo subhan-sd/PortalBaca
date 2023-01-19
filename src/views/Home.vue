ca11f91a39855c14eb3ec0857f6148a8763c9cf1
subhan-sd <subhan.singandaru2@gmail.com>
2023-01-19 15:34:00 +0700

    new check point int the middle

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto px-16 mb-4"> 
+  <div class="container mx-auto px-14 mb-4"> 
     <div class="jumbotron w-full mt-6 bg-slate-400 h-72 rounded-md " ></div>
 
     <div class="news-week flex flex-row mt-12">
@@ -20,7 +20,7 @@
       <div class="div h-96 basis-1/3 bg-gray-200 rounded-md">
 
         <div class=" bg-gray-300 h-auto rounded-sm flex-1 overflow-hidden p-3 m-6">
-        <div class=" h-16 w-16 bg-gray-800 rounded-sm float-left mr-6"></div>
+        <div class=" h-16 w-16 bg-gray-800 rounded-sm float-left mr-2"></div>
         <div class="headline ">
           <p class="title text-lg ml-4 font-semibold">Title</p>
           <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, voluptatum. Sit </p>
@@ -28,10 +28,18 @@
         </div>
 
       </div>
-      <div class="div h-auto bg-slate-300 flex-1 ml-4">
+      <div class="div h-autoflex-1 ml-4">
         <div class="text basis-1/3 m-16">
         <p class="text-3xl font-semibold ">What Hot to news this weeks ?</p>
-        <p class="font-normal">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, aspernatur tempora natus dignissimos voluptate quis possimus iusto. Assumenda, tempore perferendis?</p>
+        <p class="font-normal text-xl">Create By.Subhan || 2012, 12 Jan </p>
+        <div class="container mx-auto px-4 mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint animi quod perspiciatis aliquam ut in, soluta deleniti enim commodi voluptatum maiores autem eveniet porro vel cumque sunt nostrum est, dolorem repellat! Vel nam molestiae architecto deleniti obcaecati quas. Quaerat aliquam autem magni nemo dolore impedit sint laudantium assumenda provident eius!
+          <p class="font-light my-4 text-lg">
+            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolore eveniet quia nesciunt aliquid unde, corrupti voluptatem illum distinctio laboriosam placeat tempore nisi ipsum iste numquam fuga optio, doloremque similique. "
+          </p>
+          iv class="container mx-auto px-4 mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint animi quod perspiciatis aliquam ut in, soluta deleniti enim commodi voluptatum maiores autem eveniet porro vel cumque sunt nostrum est, dolorem repellat! Vel nam molestiae architecto deleniti obcaecati quas. Quaerat aliquam autem magni nemo dolore impedit sint laudantium assumenda provident eius!
+        </div>
         </div>
       </div>
     </div>
