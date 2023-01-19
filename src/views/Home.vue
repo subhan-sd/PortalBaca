289284ec0883212ea2366cdb710163df9d40fa2a
subhan-sd <subhan.singandaru2@gmail.com>
2023-01-19 12:04:16 +0700

    jumbo trone

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mx-auto px-4"> 
+    <div class="jumbotron w-full mt-6 bg-slate-100 h-72 rounded-sm" ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homeView',
+  props: {
+  }
+}
+</script>
+
